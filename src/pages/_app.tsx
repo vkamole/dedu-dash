28d934cb30c4b045c2adaa2d2dce1b204e46a74e
Victor Kamole Mutua <vkamole@innovexsolutions.co.ke>
2024-03-18 08:41:53 +0300

    Update font package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../styles/theme";
+import "../styles/globals.css";
+import { GoogleFonts } from "next-google-fonts";
+import Layout from "../components/Layout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const fonts = [
+  {
+    family: "Roboto Condensed",
+    weights: ["400", "500", "700"],
+  },
+];
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
+
+export default MyApp;
